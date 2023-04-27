7ef0f5e6d39c5e19119e74f9e47b7d3047ca17b1
Teriyake4 <teriyake4@outlook.com>
2023-04-26 20:10:01 -0700

    temp method to get all players from match

--- src/main/java/com/teriyake/stava/parser/MatchParser.java
+++ src/main/java/com/teriyake/stava/parser/MatchParser.java
@@ -5,14 +5,46 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class MatchParser {
+    // temp
     public static void getSegments(String jsonString) {
         Gson gson = new Gson(); // error when it gets 403?
         JsonArray initJson = gson.fromJson(jsonString, JsonObject.class)
             .get("data").getAsJsonObject()
             .get("segments").getAsJsonArray();
         for(int i = 0; i < initJson.size(); i++) {
-            System.out.println("type: " + initJson.get(i).getAsJsonObject()
+            System.out.println((i + ". ") + initJson.get(i).getAsJsonObject()
                 .get("type").getAsString());
         }
     }
+    // temp
+    public static String[] getPlayers(String jsonString) {
+        Gson gson = new Gson(); // error when it gets 403?
+        JsonArray initJson = gson.fromJson(jsonString, JsonObject.class)
+            .get("data").getAsJsonObject()
+            .get("segments").getAsJsonArray();
+        String[] players = new String[10];
+        int listIndex = 0;
+        for(int i = 0; i < initJson.size(); i++) {
+            JsonObject segment = initJson.get(i).getAsJsonObject();
+            if(segment.get("type").getAsString().equals("player-summary")) {
+                players[listIndex] = segment.get("metadata").getAsJsonObject()
+                    .get("platformInfo").getAsJsonObject()
+                    .get("platformUserIdentifier").getAsString();
+                listIndex++;
+            }
+        }
+        return players;
+    }
+
+    public static String getMatchFromRecentMatchs(String jsonString) {
+        Gson gson = new Gson();
+        String matchId = gson.fromJson(jsonString, JsonObject.class)
+            .get("data").getAsJsonObject()
+            .get("matches").getAsJsonArray()
+            .get(0).getAsJsonObject()
+            .get("attributes").getAsJsonObject()
+            .get("id").getAsString();
+        return matchId;
+    }
+
 }
