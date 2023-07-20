1c3d0e5b1a26ff8f68be516cd2cb1bf855042c82
Teriyake4 <teriyake4@outlook.com>
2023-07-20 14:57:51 -0700

    Update Round.java

--- src/main/java/com/teriyake/stava/stats/match/Round.java
+++ src/main/java/com/teriyake/stava/stats/match/Round.java
@@ -1,12 +1,12 @@
 package com.teriyake.stava.stats.match;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Round {
-    private int roundNum;
-    private String roundResult;
-    private String winningTeam;
-    private String losingTeam;
+    // private int roundNum;
+    // private String roundResult;
+    // private String winningTeam;
+    // private String losingTeam;
 
     // private ArrayList<DamageEvent> damageEvents;
     // private ArrayList<KillEvent> killEvents;
