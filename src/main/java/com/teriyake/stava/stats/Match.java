70aeef76111a52e0b71a48f36e73bc95581f2c7d
Teriyake4 <teriyake4@outlook.com>
2023-07-20 14:57:49 -0700

    Update Match.java

--- src/main/java/com/teriyake/stava/stats/Match.java
+++ src/main/java/com/teriyake/stava/stats/Match.java
@@ -1,16 +1,16 @@
 package com.teriyake.stava.stats;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-import com.teriyake.stava.stats.match.MatchData;
-import com.teriyake.stava.stats.match.Round;
-import com.teriyake.stava.stats.match.TeamSumm;
+// import com.teriyake.stava.stats.match.MatchData;
+// import com.teriyake.stava.stats.match.Round;
+// import com.teriyake.stava.stats.match.TeamSumm;
 
 public class Match {
-    private MatchData info;
-    private TeamSumm redTeam;
-    private TeamSumm blueTeam;
-    private ArrayList<Round> rounds; // includes round summaries, damage/kill events, player round summaries
+    // private MatchData info;
+    // private TeamSumm redTeam;
+    // private TeamSumm blueTeam;
+    // private ArrayList<Round> rounds; // includes round summaries, damage/kill events, player round summaries
     // private ArrayList<RoundSummary> roundSummaries; // Round results and how it was won
     // private ArrayList<DamageEvent> damageEvents; // 
     // private ArrayList<KillEvent> killEvents;
