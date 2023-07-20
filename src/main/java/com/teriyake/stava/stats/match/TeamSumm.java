e74b08405c2836089820a0222b255219a5a72b05
Teriyake4 <teriyake4@outlook.com>
2023-07-20 14:57:54 -0700

    Update TeamSumm.java

--- src/main/java/com/teriyake/stava/stats/match/TeamSumm.java
+++ src/main/java/com/teriyake/stava/stats/match/TeamSumm.java
@@ -1,15 +1,15 @@
 package com.teriyake.stava.stats.match;
 
 public class TeamSumm {
-    private String teamColor;
-    private boolean hasWon;
-    private double assists;
-    private double damage;
-    private double deaths;
-    private double kills;
-    private double roundsLost;
-    private double roundsWon;
-    private double score;
+    // private String teamColor;
+    // private boolean hasWon;
+    // private double assists;
+    // private double damage;
+    // private double deaths;
+    // private double kills;
+    // private double roundsLost;
+    // private double roundsWon;
+    // private double score;
     // private ArrayList<String> players;
 
     
