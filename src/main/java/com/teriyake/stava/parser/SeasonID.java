ff2b14692d6619618eccd651996ccf8ab55eb2f9
Teriyake4 <teriyake4@outlook.com>
2024-01-22 13:59:46 -0800

    Update SeasonID.java

--- src/main/java/com/teriyake/stava/parser/SeasonID.java
+++ src/main/java/com/teriyake/stava/parser/SeasonID.java
@@ -1,18 +1,41 @@
 package com.teriyake.stava.parser;
 
-public class SeasonID {
-    // // 5:3
-    // private static String getSeasonFromId(String seasonId) {
-        
-    // }
+import java.util.Map;
 
-    // // Episode 5 : Act 3
-    // public static String getSeasonFormated(String seasonId) {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
-    // }
-    // // 5:3
-    // public static String getSeasonRaw(String seasonId) {
+public class SeasonId {
     // https://api.tracker.gg/api/v1/valorant/db/seasons/
-        
-    // }
+    private Map<String, String> seasonIdToSeason;
+
+    public SeasonId(String seasonsJson) {
+        jsonToMap(seasonsJson);
+    }
+
+    private void jsonToMap(String seasonsJson) {
+        JsonObject episodeList = JsonParser.parseString(seasonsJson).getAsJsonObject()
+            .getAsJsonObject("data");
+        for(Map.Entry<String, JsonElement> entry : episodeList.entrySet()) {
+            JsonObject episode = entry.getValue().getAsJsonObject();
+            String episodeNum = getNumFromJson(episode);
+            JsonArray acts = episode.getAsJsonArray("seasons");
+            for(int i = 0; i < acts.size(); i++) {
+                JsonObject act = acts.get(i).getAsJsonObject();
+                String actNum = getNumFromJson(act);
+                seasonIdToSeason.put(
+                    new StringBuilder(episodeNum).append(":").append(actNum).toString(),
+                    act.get("id").getAsString());
+            }            
+        }
+        episodeList = episodeList.getAsJsonObject("0");
+    }
+
+    private String getNumFromJson(JsonObject json) {
+        return new StringBuilder(
+            json.get("shortName").getAsString())
+            .substring(1).toString();
+    }
 }
